81886a7b6046c8f78a26aa3575a7f1e02fcafde8
resteve <resteve@zikzakmedia.com>
2013-01-07 12:21:56 +0100

    Code guideline pep8

--- magento/__init__.py
+++ magento/__init__.py
@@ -6,7 +6,7 @@
     :copyright: (c) 2010 by Openlabs Technologies & Consulting (P) LTD
     :license: AGPLv3, see LICENSE for more details
 '''
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 __all__ = [
             'API',
@@ -14,15 +14,15 @@ __all__ = [
             'Country', 'Region',
             'Category', 'CategoryAttribute', 'Product', 'ProductAttribute',
             'ProductAttributeSet', 'ProductTypes', 'ProductImages',
-            'ProductTierPrice', 'ProductLinks', 'ProductConfigurable', 'Inventory',
-            'Order', 'Shipment', 'Invoice',
+            'ProductTierPrice', 'ProductLinks', 'ProductConfigurable',
+            'Inventory', 'Order', 'Shipment', 'Invoice',
             ]
 
-from api import API
-from customer import Customer, CustomerGroup, CustomerAddress
-from directory import Country, Region
-from catalog import Category, CategoryAttribute
-from catalog import Product, ProductAttribute, ProductAttributeSet
-from catalog import ProductTypes, ProductImages, ProductTierPrice
-from catalog import ProductLinks, ProductConfigurable, Inventory
-from sales import Order, Shipment, Invoice
+from .api import API
+from .customer import Customer, CustomerGroup, CustomerAddress
+from .directory import Country, Region
+from .catalog import Category, CategoryAttribute
+from .catalog import Product, ProductAttribute, ProductAttributeSet
+from .catalog import ProductTypes, ProductImages, ProductTierPrice
+from .catalog import ProductLinks, ProductConfigurable, Inventory
+from .sales import Order, Shipment, Invoice
