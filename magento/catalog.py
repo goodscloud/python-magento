cb29caa2ce2d109fc5e2302fa9a47aaeb3afc3c7
Matt Chisholm <matt@theory.org>
2013-04-13 15:54:12 +0200

    methods for ProductAttributeSet create, attributeAdd, and attributeRemove

--- magento/catalog.py
+++ magento/catalog.py
@@ -448,6 +448,41 @@ class ProductAttributeSet(API):
         """
         return self.call('catalog_product_attribute_set.list', [])
 
+    def create(self, attribute_set_name, skeleton_set_id):
+        """
+        Create a new attribute set based on a "skeleton" attribute set.
+        If unsure, use the "Default" attribute set as a skeleton.
+
+        :param attribute_set_name: name of the new attribute set
+        :param skeleton_set_id: id of the skeleton attribute set to base this set on.
+
+        :return: Integer ID of new attribute set
+        """
+        return self.call('catalog_product_attribute_set.create', [attribute_set_name, skeleton_set_id])
+
+    def attributeAdd(self, attribute_id, attribute_set_id):
+        """
+        Add an existing attribute to an attribute set.
+
+        :param attribute_id: ID of the attribute to add
+        :param attribute_set_id: ID of the attribute set to add to
+
+        :return: Boolean
+        """
+        return self.call('catalog_product_attribute_set.attributeAdd', [attribute_id, attribute_set_id])
+
+    def attributeRemove(self, attribute_id, attribute_set_id):
+        """
+        Remove an existing attribute to an attribute set.
+
+        :param attribute_id: ID of the attribute to remove
+        :param attribute_set_id: ID of the attribute set to remove from
+
+        :return: Boolean
+        """
+        return self.call('catalog_product_attribute_set.attributeRemove', [attribute_id, attribute_set_id])
+
+
 
 class ProductTypes(API):
     """
